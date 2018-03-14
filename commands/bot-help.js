b289718c88fdc56059271bdee771817cb8687d13
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-14 13:03:36 -0500

    Update bot-help.js

--- commands/bot-help.js
+++ commands/bot-help.js
@@ -1,7 +1,5 @@
 const Discord = module.require('discord.js');
 
-
-
 module.exports.run = async (bot, message, args) => {
   let botHelpText = 'The Winter server uses a custom bot to help community members with various tasks such as displaying upcoming events, joining/leaving game roles (game communities) and learning more about the Winter rank system. New features will be added as needed... and some are hidden, just waiting for unsuspecting users to stumble upon them.\n\n'
 + '**All commands should be submitted via *direct message* to** ' + /* @winter-bot */ message.guild.members.get("420054493250584586") + '.\n\n'
@@ -31,48 +29,3 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name: "help"
 }
-
-/*
-The Winter server uses a custom bot to help community members with various tasks such as displaying upcoming events, joining/leaving game roles (game communities) and learning more about the Winter rank system. New features will be added as needed... and some are hidden, just waiting for an unsuspecting user to stumble upon them.
-
-**All commands should be submitted via *direct message* to** @winter-bot.
-
-Current available commands are listed below:
-```
-help
-games
-join {game}
-leave {game}
-events
-ranks
-```
-Commands for winter-bot do not require a command prefix. DM winter-bot with `games` *not* `!games`
-
-We suggest starting with the `help` command.
-
-To request help from an actual human, please DM Freeseus or samzgr8
-
-*/
-
-/*
-CONST RICH EMBED BLOCK:
-module.exports.run = async (bot, message, args) => {
-  let embed = new Discord.RichEmbed()
-    // .setTitle()
-    // .setAuthor()
-    .setColor(0x37a0dc)
-    .setDescription(guidelinesText);
-    message.channel.send({embed: embed});
-}
-
-CONST RICH EMBED BLOCK:
-module.exports.run = async (bot, message, args) => {
-  let embed = new Discord.RichEmbed()
-    // .setTitle()
-    // .setAuthor()
-    .setColor(0x37a0dc)
-    .setDescription("text here");
-    message.channel.send({embed: embed});
-}
-
-*/
