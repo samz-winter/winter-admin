2448f9f5052647a086aa62a83931981fc5bbcbd0
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-14 13:21:35 -0500

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   let welcomeText = '**Welcome to Winter!**  For nearly 20 years, Winter has existed as a community of friends brought together by a love of gaming across genre and platform. We currently support a variety of games on both PC and PlayStation, and maintain this Discord server for communication, planning and hanging out.\n\n'
 + '**Guidelines** - We like a bit of good-natured ribbing, but we\'re not savages. You can find our full community guidelines posted in the ' + `${guidelinesChat}` + ' channel, but the short version is "Don\'t be a dick."\n\n'
 + '**winter-bot** - The Winter server uses a custom bot to manage user-joinable roles (game communities) and provide some comic relief now and then. You can find more information about how to use winter-bot in the ' + `${botHelpChat}` + ' channel, or you can DM ' + `${winterBot}` + ' with the command `help`\n\n'
-+ '**Text Channels** - Text channels are open to all users. Drop in and introduce yourself if you\'d like.\n'
++ '**Text Channels** - Text channels are open to all users. Drop in and introduce yourself!\n'
 + '• ' + `${winterGeneralChat}` + ' is our off-topic/hangout chat. Expect shenanigans... and probably lots of "dad jokes."\n'
 + '• All other text channels are labeled by topic. Please keep conversations in their appropriate channels (e.g. anything about Monster Hunter should be posted in ' + `${monsterHunterChat}` + '; anything about music should be posted in ' + `${musicChat}` + ', etc).\n\n'
 + '**Voice Channels** - Voice channels can be used as needed. Just don\'t hang out in there alone; people will think you\'re weird.\n\n'
