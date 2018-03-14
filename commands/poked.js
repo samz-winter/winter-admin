3174d485f68f8e7f98dd93540095cc8bc96e613d
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-14 11:50:05 -0500

    Update poked.js

--- commands/poked.js
+++ commands/poked.js
@@ -1,12 +1,13 @@
 const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-  let pokedFooter = 'Disclaimer: This is not an *official* timeout ...but you probably pissed off an officer. Your privileges will be restored shortly.'
+  let pokedFooter = 'Disclaimer: This is not an *official* timeout \n...but you probably pissed off an officer. \nYour privileges will be restored shortly.'
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
     // .setImage("https://78.media.tumblr.com/226579e43254866dad8de897c22f4dfd/tumblr_inline_o9rafdoghX1te58zi_320.gif")
     .setImage("https://www.reactiongifs.com/r/ksu.gif")
-    .setFooter(pokedFooter);
+    .setDescription(pokedFooter);
+    // .setFooter(pokedFooter);
     message.channel.send({embed: embed});
   }
 
