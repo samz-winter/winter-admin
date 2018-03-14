7f4c3ddc5f5a99bdb6835cff07cab5b0275e9b63
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-14 13:06:33 -0500

    Update timeout.js

--- commands/timeout.js
+++ commands/timeout.js
@@ -9,50 +9,12 @@ module.exports.run = async (bot, message, args) => {
 // + /* #guidelines */ message.guild.channels.get("413675044435394570")
 + `${guidelines}`
   
-  // BEGIN RICH EMBED
   let embed = new Discord.RichEmbed()
-    // .setTitle()
-    // .setAuthor()
     .setColor(0x37a0dc)
     .setDescription(timeoutText);
     message.channel.send({embed: embed});
-    // message.channel.send("★  https://youtu.be/ksBE53CIT8E");
-
-    // message.channel.send("https://youtu.be/ksBE53CIT8E");
-}
+  }
 
 module.exports.help = {
   name: "timeout"
 }
-
-/*
-
-CONST RICH EMBED BLOCK:
-module.exports.run = async (bot, message, args) => {
-  //console.log("guidelines");
-  let embed = new Discord.RichEmbed()
-    // .setTitle()
-    // .setAuthor()
-    .setColor(0x37a0dc)
-    .setDescription(guidelinesText);
-    message.channel.send({embed: embed});
-}
-
-CONST RICH EMBED BLOCK:
-module.exports.run = async (bot, message, args) => {
-  let embed = new Discord.RichEmbed()
-    // .setTitle()
-    // .setAuthor()
-    .setColor(0x37a0dc)
-    .setDescription("text here");
-    message.channel.send({embed: embed});
-}
-
-let guidelines = message.guild.channels.get("413675044435394570");
-bot.sendMessage(message, `${adminRoleObject} are on the way!`);
-
-let message.guild.channels.get("413675044435394570")
-
-!timeout
-
-*/
