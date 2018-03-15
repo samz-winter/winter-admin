e1f135d49a64ac0aa685aa98083927f25179a5a2
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-15 09:16:57 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -7,10 +7,14 @@ module.exports.run = async (bot, message, args) => {
   let testText = '::gives ' + `<@!` + message.author.id + `>` + ' the side-eye.::'
   let testText2 = `${samzUser} is cool.`
   let testText3 = '**Channel to be used for BOT log of member join/leave activity, promotions and kick/ban log.**'
+  
+  let burnoutText = `"Ya know there is lots of stuff to do in Paradise City. Outside all that Racing and Road Raging stuff. Go take out some Burnout Billboards, break some Road Rules, find some new shortcuts, or something big to jump off! Let's explore, people! Know your enemy, and know your battlefield! This is Atomika, on Crash FM!"`
+  let burnoutFooter = '-DJ Atomika'
   // BEGIN RICH EMBED
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
-    .setDescription(testText3);
+    .setDescription(burnoutText)
+    .setFooter(burnoutFooter);
     message.channel.send({embed: embed});
     // message.channel.send(testText2);
 }
