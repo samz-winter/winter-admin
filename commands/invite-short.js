21a6b947c4358f5827609f3215108f5c54fd8362
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-15 09:48:33 -0500

    Create invite-short.js

---
+++ commands/invite-short.js
@@ -0,0 +1,15 @@
+const Discord = module.require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+  let inviteText = 'Invite link updated MARCH.14.2018\n\n'
++ 'The previous link has been expired.\n'
++ '**Please refresh recruiting posts.**'
+  let embed = new Discord.RichEmbed()
+    .setColor(0x37a0dc)
+    .setDescription(inviteText)
+    message.channel.send("★  https://discord.gg/ExUTWT4  ★", {embed: embed});
+  }
+
+module.exports.help = {
+  name: "invite-short"
+}
