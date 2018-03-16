f4abff5274071a605a15d2d065ac2aab1bed09fa
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-16 16:57:06 -0500

    Update setgame.js

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,9 +1,10 @@
 const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
+  let samzgr8 = bot.users.get("111980412296839168")
   var argsResult = args.join(' ');
   
-  bot.user.setGame(argsResult);
+  message.author.setGame(argsResult);
 }
 
 module.exports.help = {
