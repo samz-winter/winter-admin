b78a5a4961006665fccfbcd1f40c6749366ca252
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-16 18:31:38 -0500

    Update shutup.js

--- commands/shutup.js
+++ commands/shutup.js
@@ -3,8 +3,8 @@ const Discord = module.require('discord.js');
 module.exports.run = async (bot, message, args) => {
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  let adminLog = message.guild.channels.get("423825546087235585");
-  let muteRole = message.guild.roles.get("420581883638972432");
+  let adminLog = message.guild.channels.get("423825546087235585"); // #poked_the_bear
+  let muteRole = message.guild.roles.get("420581883638972432"); // @shutup
 
   if(!adminLog) return message.reply('I cannot find an Admin Log channel.').catch(console.error);
   if(!muteRole) return message.reply('I cannot find a Mute role.').catch(console.error);
@@ -13,7 +13,7 @@ module.exports.run = async (bot, message, args) => {
 
   message.guild.member(user).addRole(muteRole).then(() => {
     message.channel.send(`Now ${user} knows what happens when you annoy an officer.`);
-    adminLog.send(`${user} has been told to shut-up by` + `<@!` + message.author.id + `>`);
+    adminLog.send(`${user} has been told to shut-up by ` + `<@!` + message.author.id + `>`);
   });
 }
 
