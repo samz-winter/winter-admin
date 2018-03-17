015adedafc497c6159f5f5162d57c59413485630
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-16 21:55:00 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -7,6 +7,7 @@ module.exports.run = async (bot, message, args) => {
   let testText = '::gives ' + `<@!` + message.author.id + `>` + ' the side-eye.::'
   let testText2 = `${samzUser} is cool.`
   let testText3 = '**Channel to be used for BOT log of member join/leave activity, promotions and kick/ban log.**'
+  let testText4 = `You're not cool.`
   
   let burnoutText = `"Ya know there is lots of stuff to do in Paradise City. Outside all that Racing and Road Raging stuff. Go take out some Burnout Billboards, break some Road Rules, find some new shortcuts, or something big to jump off! Let's explore, people! Know your enemy, and know your battlefield! This is Atomika, on Crash FM!"`
   let burnoutFooter = '-DJ Atomika'
@@ -15,8 +16,8 @@ module.exports.run = async (bot, message, args) => {
     .setColor(0x37a0dc)
     .setDescription(burnoutText)
     .setFooter(burnoutFooter);
-    message.channel.send({embed: embed});
-    // message.channel.send(testText2);
+    // message.channel.send({embed: embed});
+    message.channel.send(testText4);
 }
 
 module.exports.help = {
