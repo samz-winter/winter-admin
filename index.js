0acf601bf58d4cb4fff31a88d86086dbee4002d4
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-17 08:14:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,14 +37,14 @@ bot.on('message', async message => {
 
   let samzID = '111980412296839168';
   let benzID = '213091147059429393';
-  let officerRole = message.author.roles.get("211143245445464064");
+  // let officerRole = message.author.roles.get("211143245445464064");
 
   let errorText = `<@!` + message.author.id + `>` + ` Please DM ${winterBot} if you need help.`
 
   if(!msg.startsWith(prefix)) return;
   if(message.author.bot) return;
   // if message contains command prefix and author not samzgr8
-  if(msg.startsWith(prefix) && !officerRole) {
+  if(msg.startsWith(prefix) && !message.member.roles.get("211143245445464064")) {
     // message.author.send(errorText);
     // message.channel.send(errorText);
     return;
