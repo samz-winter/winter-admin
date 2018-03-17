8be2b0996d2f9e26ed2ebb6ad2688bd9c4b952aa
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-17 10:00:39 -0500

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -1,16 +1,16 @@
 const Discord = module.require('discord.js');
-const botSettings = require('./responses.json');
-const testText = responses.testText;
-
+const responses = require('./responses.json');
 
 module.exports.run = async (bot, message, args) => {
   let samzUser = bot.users.get("111980412296839168");
   let emojiBlocked = message.guild.roles.get("421358575458451457");
   
-  let testText = '::gives ' + `<@!` + message.author.id + `>` + ' the side-eye.::'
+  let testText1 = '::gives ' + `<@!` + message.author.id + `>` + ' the side-eye.::'
   let testText2 = `${samzUser} is cool.`
   let testText3 = '**Channel to be used for BOT log of member join/leave activity, promotions and kick/ban log.**'
   let testText4 = `You're not cool.`
+  
+  let testText = responses.testText;
   
   let burnoutText = `"Ya know there is lots of stuff to do in Paradise City. Outside all that Racing and Road Raging stuff. Go take out some Burnout Billboards, break some Road Rules, find some new shortcuts, or something big to jump off! Let's explore, people! Know your enemy, and know your battlefield! This is Atomika, on Crash FM!"`
   let burnoutFooter = '-DJ Atomika'
