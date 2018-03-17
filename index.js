9cfa0781a45c326b2f2d0264a5b845aea1887151
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-17 08:22:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ bot.on('message', async message => {
 
   let samzID = '111980412296839168';
   let benzID = '213091147059429393';
-  let officerRole = message.guild.roles.get("211143245445464064");
+  let officerRole = message.member.roles.has("211143245445464064");
 
   let errorText = `<@!` + message.author.id + `>` + ` Please DM ${winterBot} if you need help.`
 
