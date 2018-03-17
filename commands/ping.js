96e78c3f61aa52e68fa7e4343ce0f56c47dccd3d
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-17 12:35:10 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -7,30 +7,30 @@ module.exports.run = async (bot, message, args) => {
   let reason = args.slice(1).join(' ');
   let adminLog = message.guild.channels.get("423825546087235585"); // #admin_log
   let directMessage = message.guild.member(user);
-  let monsterHunterChat = message.guild.channels.get("407697254422413322"); // #monster_hunter
-  let musicChat = message.guild.channels.get("414137151467421697"); // #music
 
-  // if(message.mentions.users.size < 1) return message.reply('You must mention someone to ping.').catch(console.error);
-  // if(reason.length < 1) return message.reply('You must enter a reason.').catch(console.error);
+  if(message.mentions.users.size < 1) return message.reply('You must mention someone to ping.').catch(console.error);
+  if(reason.length < 1) return message.reply('You must enter a reason.').catch(console.error);
   if(!adminLog) return message.reply('I cannot find an Admin Log channel.').catch(console.error);
-  
+
+  let communitiesPing = '• **Winter Game Communities**'
+  let guidelinesPing = '• **Winter Community Guidelines**'
   let voicePing = '• **Winter Community Voice Channels** are reserved for community members gaming *together* or participating in community social events. In order to keep as many channels available as possible, we ask that members **do not hang out in Gaming channels alone.**\n\n'
 + 'Please move to one of the Lobby channels, one of the LFG channels, or the AFK channel.\n\n'
 + '**Repeated reminders may result in the loss of your Voice Channel privileges.**'
   
-  let textPing = '• **Winter Community Text Channels** are titled by topic.  Please keep conversations in their appropriate channels (e.g. anything about Monster Hunter should be posted in ' + `${monsterHunterChat}` + '; anything about music should be posted in ' + `${musicChat}` + ', etc).'
-
-  let guidelinesPing = '• **Winter Community Guidelines**'
   
   if(msg.startsWith(prefix + "ping")) {
     if(reason === "voice") {
-      if(message.mentions.users.size < 1) return message.reply('You must mention someone to ping.').catch(console.error);
       directMessage.send(voicePing);
       adminLog.send(`<@!` + message.author.id + `>` + ` has pinged ${user} \nRe: Voice Channels`);
     return;
     }else if(reason === "guidelines") {
       directMessage.send(guidelinesPing);
       adminLog.send(`<@!` + message.author.id + `>` + ` has pinged ${user} \nRe: Guidelines`);
+      return;
+    }else if(reason === "communities") {
+      directMessage.send(communitiesPing);
+      adminLog.send(`<@!` + message.author.id + `>` + ` has pinged ${user} \nRe: Joining Communities`);
       return;
     }else {
       let embed = new Discord.RichEmbed()
@@ -39,11 +39,6 @@ module.exports.run = async (bot, message, args) => {
         // message.channel.send({embed: embed});
       message.author.send(embed);
     };
-  }
-
-  if(msg.startsWith(prefix + "ping text")) {
-    message.channel.send(textPing);
-    return;
   }
 }
 
