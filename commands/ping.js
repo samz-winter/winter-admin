25eb30c1e07bbe9a9500edb7a38faf18ec241aab
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-17 08:11:06 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -11,23 +11,25 @@ module.exports.run = async (bot, message, args) => {
   if(message.mentions.users.size < 1) return message.reply('You must mention someone to mute.').catch(console.error);
   if(reason.length < 1) return message.reply('You must enter a reason.').catch(console.error);
   if(!adminLog) return message.reply('I cannot find an Admin Log channel.').catch(console.error);
-  if(reason !== "voice" || "text")
+  if(reason !== "voice" || "text") {
+    message.author.send("The following reasons can be used for pinging a user\n```\nvoice\ntext\n```\n."
+  };
 
   if(msg.startsWith(prefix + "ping") && reason === "voice") {
     directMessage.send(`${user} should read the voice channel guidelines.`);
-    adminLog.send(`${user} has been warned via DM Re: Voice Channels by ` + `<@!` + message.author.id + `>`);
+    adminLog.send(`${user} has been pinged via DM Re: Voice Channels by ` + `<@!` + message.author.id + `>`);
     // message.author.send(errorText);
     // message.channel.send(errorText);
     return;
-    };
+  };
 
   if(msg.startsWith(prefix + "ping") && reason === "text") {
     directMessage.send(`${user} should read the text channel guidelines.`);
-    adminLog.send(`${user} has been warned via DM Re: Text Channels by ` + `<@!` + message.author.id + `>`);
+    adminLog.send(`${user} has been pinged via DM Re: Text Channels by ` + `<@!` + message.author.id + `>`);
     // message.author.send(errorText);
     // message.channel.send(errorText);
     return;
-    };
+  };
 }
 
 module.exports.help = {
