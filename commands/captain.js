4d363a8a3d060f2e3cc5117900e80fbe4a4209d3
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-19 08:36:52 -0500

    Update captain.js

--- commands/captain.js
+++ commands/captain.js
@@ -6,20 +6,24 @@ module.exports.run = async (bot, message, args) => {
   let samzID = bot.users.get("111980412296839168");
   let benzID = bot.users.get("213091147059429393");
 
-  let captainText = '**THIS IS A READ-ONLY CHANNEL**\n\n'
-+ 'After careful consideration and planning, Winter leadership has added a `quarantine` command to winter-bot which will allow any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned.\n\n'
-+ 'This command is *only* to be used for situations where a user needs to be silenced immediately.\n\n'
-+ '***YOUR ONE WARNING:*** Anyone who uses this to troll or try to be funny *will* be banned from the server. Period.\n\n'
-+ 'If this command is used, please **DM** details of the situation to ' + `${samzID}` + '.\n\n'
-+ 'Instructions for the command have been added to the winter-bot help command for captains, but basically, **DM** ' + `${winterBot}` + ' with the following:\n\n'
-+ '`quarantine {discordtag#0000}`\n\n'
-+ 'e.g. `quarantine samzgr8#0513`\n'
+let inviteLinkText = '★  https://discord.gg/ExUTWT4  ★\n'
++ '```diff\n'
++ '- Invite link updated MARCH.14.2018\n'
++ '- Please refresh all active recruiting posts.\n'
++ '```'
 
-  // BEGIN RICH EMBED
+let captainText = '**THIS IS A READ-ONLY CHANNEL. For issues, please send a DM to** ' + `${samzID}` + '.\n\n'
++ 'The winter-bot `quarantine` command allows any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned. **This command should *only* be used for situations where a user needs to be silenced immediately.**\n\n'
++ 'If this command is used, please **send a DM with details** of the situation to ' + `${samzID}` + '.\n\n'
++ 'To use the winter-bot `quarantine` command, **send a DM to** ' + `${winterBot}` + ' with the following:\n\n'
++ '`quarantine {discordtag#0000}`\n\n'
++ 'e.g. `quarantine samzgr8#0513`\n\n'
++ '***WARNING:*** Anyone who uses this to troll or try to be funny *will* be banned from the server. Period.'
+
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
     .setDescription(captainText);
-    message.channel.send({embed: embed});
+    message.channel.send(inviteLinkText, {embed: embed});
 }
 
 module.exports.help = {
