f3a3230f2174e78a1f8a9e8677d8d23541e9935a
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-19 12:52:01 -0500

    Update invite-short.js

--- commands/invite-short.js
+++ commands/invite-short.js
@@ -1,21 +1,11 @@
 const Discord = module.require('discord.js');
+const textblocks = require('./textblocks/textblocks.js');
 
 module.exports.run = async (bot, message, args) => {
-  let linkText = '★  https://discord.gg/ExUTWT4  ★\n'
-+ '```diff\n'
-+ '- Invite link updated MARCH.14.2018\n'
-+ '- Please refresh all active recruiting posts.\n'
-+ '```\n'
-  
-  let inviteText = 'Invite link updated MARCH.14.2018\n'
-+ 'The previous link is no longer valid.\n\n'
-+ '**Please refresh all active recruiting posts.**'
-  
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
-    .setDescription(inviteText)
-    // message.channel.send(linkText, {embed: embed});
-    message.channel.send(linkText);
+    .setDescription(textblocks.linkText)
+    message.channel.send(linkText, {embed: embed});
   }
 
 module.exports.help = {
