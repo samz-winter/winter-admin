6f0f4e5448615ca08a7f30ff0152a967baa47e85
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-19 12:48:41 -0500

    Create textblocks.js

---
+++ commands/textblocks/textblocks.js
@@ -0,0 +1,15 @@
+module.exports = {
+  inviteLinkText: '★  https://discord.gg/ExUTWT4  ★\n'
++ '```diff\n'
++ '- Invite link updated MARCH.14.2018\n'
++ '- Please refresh all active recruiting posts.\n'
++ '```\n'
+
+  captainText: '**THIS IS A READ-ONLY CHANNEL. For issues, please send a DM to** ' + `${samzID}` + '.\n\n'
++ 'The winter-bot `quarantine` command allows any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned. **This command should *only* be used for situations where a user needs to be silenced immediately.**\n\n'
++ 'If this command is used, please **send a DM with details** of the situation to ' + `${samzID}` + '.\n\n'
++ 'To use the winter-bot `quarantine` command, **send a DM to** ' + `${winterBot}` + ' with the following:\n\n'
++ '`quarantine {discordtag#0000}`\n\n'
++ 'e.g. `quarantine samzgr8#0513`\n\n'
++ '***WARNING:*** Anyone who uses this to troll or try to be funny *will* be banned from the server. Period.'
+};
