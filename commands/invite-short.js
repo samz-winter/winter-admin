58a138e6eca398654c228719fa79cd7f18d5b82a
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-19 08:30:05 -0500

    Update invite-short.js

--- commands/invite-short.js
+++ commands/invite-short.js
@@ -6,14 +6,16 @@ module.exports.run = async (bot, message, args) => {
 + '- Invite link updated MARCH.14.2018\n'
 + '- Please refresh all active recruiting posts.\n'
 + '```\n'
+  
   let inviteText = 'Invite link updated MARCH.14.2018\n'
 + 'The previous link is no longer valid.\n\n'
 + '**Please refresh all active recruiting posts.**'
+  
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
     .setDescription(inviteText)
-    // message.channel.send("★  https://discord.gg/ExUTWT4  ★", {embed: embed});
-    message.channel.send(linkText, {embed: embed});
+    // message.channel.send(linkText, {embed: embed});
+    message.channel.send(linkText);
   }
 
 module.exports.help = {
