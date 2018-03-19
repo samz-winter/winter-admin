920e3afdb439a10b10ef067a2a998454d38f046a
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-19 13:07:42 -0500

    Update invite-short.js

--- commands/invite-short.js
+++ commands/invite-short.js
@@ -4,8 +4,8 @@ const textblocks = require('./textblocks/textblocks.js');
 module.exports.run = async (bot, message, args) => {
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
-    .setDescription(textblocks.linkText)
-    message.channel.send(linkText, {embed: embed});
+    .setDescription(textblocks.captainText)
+    message.channel.send({embed: embed});
   }
 
 module.exports.help = {
