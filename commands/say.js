4b435b76386026283bc21c5626e0ab82897c9bfa
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-22 11:47:26 -0500

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,7 +1,7 @@
 const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-  let text = args.slice(1).join(" ");
+  let text = args.slice(0).join(" ");
   message.delete();
   message.channel.send(text);
 }
