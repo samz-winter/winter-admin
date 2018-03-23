44871fb3e9ca7ed75e90e65f2356aa70ec02bf97
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 11:51:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -39,13 +39,15 @@ bot.on('message', async message => {
   let samzID = '111980412296839168';
   let benzID = '213091147059429393';
   // let officerRole = message.guild.roles.get("211143245445464064");
-
+  let samzUser = bot.users.get("111980412296839168");
+  
   let errorText = `<@!` + message.author.id + `>` + ` Please DM ${winterBot} if you need help.`
 
   if(!msg.startsWith(prefix)) return;
   if(message.author.bot) return;
   // if message contains command prefix and author not winterOfficer
-  if(msg.startsWith(prefix) && !message.member.roles.has("211143245445464064")) return;
+  if(msg.startsWith(prefix) && !message.member.roles.has("211143245445464064")) return; // prefix and not winterOfficer
+  if(msg.startsWith(prefix) && !bot.users.get("111980412296839168")) return; // prefix and not samzgr8
 
 /* NOT WORKING
   // if message mentions winter-admin and author not samzgr8
