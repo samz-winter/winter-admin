668efdb55280ebeec9d81eb3e0bc5aab8db031bc
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 11:29:44 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -3,6 +3,6 @@ exports.testText = function(message){
     // text: 'This is a test/n' + `${samzUser} is cool.`,
     text: 'This is a test/n' + `Sam is cool.`,
     img: 'http://www.reactiongifs.com/r/magnum.gif',
-    footer: [`${samzUser} is cool.`, '']
+    // footer: [`${samzUser} is cool.`, '']
   }
 };
