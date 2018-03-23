4d30003473527ece6c3e046bf620ccd31b88b5a7
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 11:52:34 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -2,7 +2,7 @@ exports.testText = function(bot, message, args){
   // const discordjs = require('discord.js');
   // let samzUser = bot.users.get("111980412296839168"); // @samzgr8
   return {
-    text: `${message.author.username} was coding.\nBanging head against the desk\nWill it ever work?`,
+    text: `Samzgr8 was coding.\nBanging head against the desk\nWill it ever work?`,
     // text: 'This is a test\n' + `Sam is cool.`,
     // img: 'http://www.reactiongifs.com/r/magnum.gif',
     footer: [`-samzgr8, 2018`, '']
