d8e3284f15fff40fee52aa2f3953c68f9d9b9393
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 16:31:41 -0500

    Create textxxx.js

---
+++ commands/textblocks/textxxx.js
@@ -0,0 +1,10 @@
+const textxxx = {
+
+  "testText" : {
+    "text": "{{samzUser}} was coding.\nBanging head against the desk\nWill it ever work?", // ${samzUser}
+    // "img": "http://www.reactiongifs.com/r/magnum.gif",
+    "footer": ["-samzgr8, 2018", ""]
+  },
+};
+
+module.exports = textxxx;
