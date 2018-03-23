b57762c0f93fb986c75d7ea2a2ad7feb72284f4a
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 17:10:13 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -1,6 +1,6 @@
 module.exports.testText = function (bot, message, args) {
   return {
-    samzUser: bot.users.find("id", "111980412296839168")
+    samzUser: bot.users.find("id", "111980412296839168"),
     text: `${samzUser} was coding.\nBanging head against the desk\nWill it ever work?`, // ${samzUser}
     // img: 'http://www.reactiongifs.com/r/magnum.gif',
     footer: [`-samzgr8, 2018`, '']
