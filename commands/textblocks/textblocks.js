cb92aadb05158e0bb1b9d97df24ec555ab9a5194
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 17:41:45 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -1,7 +1,7 @@
 module.exports.testText = function (bot, message, args) {
   // console.log(bot, message, args);
   return {
-    let samzUser = bot.users.get('111980412296839168');
+    let samzUser = bot.users.get('111980412296839168')
     // let samzUser = bot;
     // console.log(samzUser.users);
     text: [`${samzUser} was coding.\nBanging head against the desk\nWill it ever work?`], // ${samzUser}
