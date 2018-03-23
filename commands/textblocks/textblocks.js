3407dd70d2db860c0c94f85aa58a8149d2d147b6
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 10:28:52 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -1,22 +1,7 @@
-const Discord = module.require('discord.js');
-
-module.exports = {
-  inviteLinkText: '★  https://discord.gg/ExUTWT4  ★\n'
-+ '```diff\n'
-+ '- Invite link updated MARCH.14.2018\n'
-+ '- Please refresh all active recruiting posts.\n'
-+ '```\n'
-};
-
-module.exports.run = async (bot, message, args) => {
-  const winterBot = message.guild.members.get("420054493250584586"); // @winter-bot
-  const samzID = message.guild.members.get("111980412296839168"); // @samzgr8
-  
-  captainText: '**THIS IS A READ-ONLY CHANNEL. For issues, please send a DM to** ' + `${samzID}` + '.\n\n'
-+ 'The winter-bot `quarantine` command allows any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned. **This command should *only* be used for situations where a user needs to be silenced immediately.**\n\n'
-+ 'If this command is used, please **send a DM with details** of the situation to ' + `${samzID}` + '.\n\n'
-+ 'To use the winter-bot `quarantine` command, **send a DM to** ' + `${winterBot}` + ' with the following:\n\n'
-+ '`quarantine {discordtag#0000}`\n\n'
-+ 'e.g. `quarantine samzgr8#0513`\n\n'
-+ '***WARNING:*** Anyone who uses this to troll or try to be funny *will* be banned from the server. Period.'
+exports.testText = function(message){
+	return {
+		text: 'This is a test/n' + `${samzUser} is cool.`,
+    img: 'http://www.reactiongifs.com/r/magnum.gif',
+		footer: [`${samzUser} is cool.`, '']
+	}
 };
