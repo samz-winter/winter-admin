3255b3591ada06aa1598bb86142a1e61f0135fea
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-23 18:01:48 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -1,10 +1,15 @@
 module.exports.testText = function (bot, message, args) {
-  const samzUser = bot.users.get('111980412296839168');
-  // console.log(bot, message, args);
+  let samzUser = bot.users.get('111980412296839168');
   return {
-    text: [`${samzUser} was coding.\nBanging head against the desk\nWill it ever work?`], // ${samzUser}
+    text: [`${samzUser} was coding.\nBanging head against the desk\nWill it ever work?`],
     // img: 'http://www.reactiongifs.com/r/magnum.gif',
     footer: [`-samzgr8, 2018`, '']
+  }
+};
+
+module.exports.iInviteLink = function (bot, message, args) {
+  return {
+    text: ['★  https://discord.gg/ExUTWT4  ★\n```diff\n- Invite link updated MARCH.14.2018\n```']
   }
 };
 
