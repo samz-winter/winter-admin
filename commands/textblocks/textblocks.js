e05f63412db20f03009d7ef29c57e5ce357d8737
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-26 10:32:03 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -1,7 +1,8 @@
 module.exports.testText = function (bot, message, args) {
   let samzUser = bot.users.get('111980412296839168');
   return {
-    text: [`${samzUser} was coding.\nBanging head against the desk\nWill it ever work?`],
+    text: `${samzUser} was coding.\n`
+    + `Banging head against the desk\nWill it ever work?`],
     // img: 'http://www.reactiongifs.com/r/magnum.gif',
     footer: [`-samzgr8, 2018`, '']
   }
@@ -9,8 +10,14 @@ module.exports.testText = function (bot, message, args) {
 
 module.exports.inviteLink = function (bot, message, args) {
   return {
-    text: ['★  https://discord.gg/ExUTWT4  ★\n```diff\n- Invite link updated MARCH.14.2018\n```']
+    text: ['★  https://discord.gg/Xsf6qTQ  ★\n```diff\n- Invite link updated MARCH.26.2018\n```']
   }
 };
 
-
+module.exports.shutupPost = function (bot, message, args) {
+  return {
+    text: '**This is what happens when you annoy an officer.**',
+    img: 'https://www.reactiongifs.com/r/ksu.gif',
+    footer: ['Your privileges will be restored shortly.', '']
+  }
+};
