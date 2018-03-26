e13aa64e8dc1240de31035cea30d51f21490e0f6
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-26 11:30:30 -0500

    Update textblocks.js

--- commands/textblocks/textblocks.js
+++ commands/textblocks/textblocks.js
@@ -1,6 +1,7 @@
 const assets = require('./assets.js').assets;
 
 module.exports.testText = function (bot, message, args) {
+  console.log(`${assets.samzUser}`);
   return {
     text: `${assets.samzUser} was coding.\n`
     + `Banging head against the desk\nWill it ever work?`,
