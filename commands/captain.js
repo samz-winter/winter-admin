5bd5baf8d9aefc87a9bd8250d844e8a11d191383
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-26 08:18:33 -0500

    Update captain.js

--- commands/captain.js
+++ commands/captain.js
@@ -9,16 +9,13 @@ module.exports.run = async (bot, message, args) => {
 let inviteLinkText = '★  https://discord.gg/ExUTWT4  ★\n'
 + '```diff\n'
 + '- Invite link updated MARCH.14.2018\n'
-+ '- Please refresh all active recruiting posts.\n'
 + '```'
 
 let captainText = '**THIS IS A READ-ONLY CHANNEL. For issues, please send a DM to** ' + `${samzID}` + '.\n\n'
-+ 'The winter-bot `quarantine` command allows any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned. **This command should *only* be used for situations where a user needs to be silenced immediately.**\n\n'
-+ 'If this command is used, please **send a DM with details** of the situation to ' + `${samzID}` + '.\n\n'
-+ 'To use the winter-bot `quarantine` command, **send a DM to** ' + `${winterBot}` + ' with the following:\n\n'
-+ '`quarantine {discordtag#0000}`\n\n'
++ 'The winter-bot `quarantine` command allows any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned. **This command should *only* be used for situations where a user needs to be silenced immediately.** Please **send a DM with details** of the situation to ' + `${samzID}` + ' if this command is used, .\n\n'
++ 'To use the winter-bot `quarantine` command, **send a DM to** ' + `${winterBot}` + ' with the following:  `quarantine {discordtag#0000}`\n\n'
 + 'e.g. `quarantine samzgr8#0513`\n\n'
-+ '***WARNING:*** Anyone who uses this to troll or try to be funny *will* be banned from the server. Period.'
++ '***WARNING:*** Anyone who intentionally misuses this command *will* be banned from the server. Period.'
 
   let embed = new Discord.RichEmbed()
     .setColor(0x37a0dc)
