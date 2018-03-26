8ee295843c2995e7b91ef3c4fbf52c6664873a59
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-26 08:23:41 -0500

    Update captain.js

--- commands/captain.js
+++ commands/captain.js
@@ -8,10 +8,10 @@ module.exports.run = async (bot, message, args) => {
 
 let inviteLinkText = '★  https://discord.gg/ExUTWT4  ★\n'
 + '```diff\n'
-+ '- Invite link updated MARCH.14.2018\n'
++ '- Invite link updated MARCH.26.2018\n'
 + '```'
 
-let captainText = '**THIS IS A READ-ONLY CHANNEL. For issues, please send a DM to** ' + `${samzID}` + '.\n\n'
+let captainText = '**THIS IS A READ-ONLY CHANNEL. For issues, please  DM ** ' + `${samzID}` + '.\n\n'
 + 'The winter-bot `quarantine` command allows any community captain to temporarily block a user\'s server privileges until an officer can decide if the user should be kicked/banned. **This command should *only* be used for situations where a user needs to be silenced immediately.** Please **send a DM with details** of the situation to ' + `${samzID}` + ' if this command is used.\n\n'
 + 'To use the winter-bot `quarantine` command, **send a DM to** ' + `${winterBot}` + ' with the following:  `quarantine {discordtag#0000}`\n\n'
 + 'e.g. `quarantine samzgr8#0513`\n\n'
