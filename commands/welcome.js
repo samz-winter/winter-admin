2d02f6271e2ab92f2303cd5537eb169beadda2fa
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-26 09:23:51 -0500

    Update welcome.js

--- commands/welcome.js
+++ commands/welcome.js
@@ -10,15 +10,12 @@ module.exports.run = async (bot, message, args) => {
   let captainRLRole = message.guild.roles.get("414098252569378816")
 
   let welcomeText = '**Welcome to Winter!**  For nearly 20 years, Winter has existed as a community of friends brought together by a love of gaming across genre and platform. We currently support a variety of games on both PC and PlayStation, and maintain this Discord server for communication, planning and hanging out.\n\n'
-+ '**Guidelines** - We like a bit of good-natured ribbing, but we\'re not savages. You can find our full community guidelines posted in the ' + `${guidelinesChat}` + ' channel, but the short version is "Don\'t be a dick."\n\n'
-+ '**winter-bot** - The Winter server uses a custom bot to manage user-joinable roles (game communities) and provide some comic relief now and then. You can find more information about how to use winter-bot in the ' + `${botHelpChat}` + ' channel, or you can DM ' + `${winterBot}` + ' with the command `help`\n\n'
-+ '**Text Channels** - Text channels are open to all users. Drop in and introduce yourself!\n'
-+ '• ' + `${winterGeneralChat}` + ' is our off-topic/hangout chat. Expect shenanigans... and probably lots of "dad jokes."\n'
-+ '• All other text channels are labeled by topic. Please keep conversations in their appropriate channels (e.g. anything about Monster Hunter should be posted in ' + `${monsterHunterChat}` + '; anything about music should be posted in ' + `${musicChat}` + ', etc).\n\n'
-+ '**Voice Channels** - Voice channels can be used as needed. Just don\'t hang out in there alone; people will think you\'re weird.\n\n'
++ `**Guidelines** - We like a bit of good-natured ribbing, but we\'re not savages. You can find our full community guidelines posted in the ${guidelinesChat} channel, but the short version is "Don\'t be a dick."\n\n`
++ `**winter-bot** - The Winter server uses a custom bot to manage user-joinable roles (game communities) and provide some comic relief now and then. **You can find more information about how to use winter-bot by sending a direct message to ${winterBot} with the word "help"**\n\n`
 + '**Game Communities** - Winter encourages members to organize into communities based on the games they play. Active games with a reasonable community size will usually have their own text channel, and each community has at least one Captain who acts as the advocate, recruiter and organizer.\n'
-+ '• If you need to contact a community Captain, you can mention them by role (e.g. ' + `${captainRLRole}` + ').\n'
-+ '• See ' + `${botHelpChat}` + ' for instructions on how to add yourself to a game community.\n\n'
++ '  • All server users need to add themselves to at least one community role. This allows the community captains to @ mention the community as a whole for important updates.\n'
++ '  • If you do not want game @ mentions at all (for any game community), please add yourself to the role @no_mentions. If you do not, an officer will be adding you to any community they see you active in.\n'
++ `  • **For instructions on how to add yourself to a community, send a direct message to** ${winterBot} **with the word "games".**\n\n`
 + 'Visit Winter\'s website at [winterclan.net](https://www.winterclan.net) and use your Discord credentials to log in.\n'
 
   // BEGIN RICH EMBED
