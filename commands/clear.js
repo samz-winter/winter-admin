37031de0143bf7e2b1a005e638c9b1e8759b1cb5
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-27 10:49:03 -0500

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   if(!args[0]) return message.channel.send("Enter number of messages to clear.");
   message.delete().then(() => {
     message.channel.bulkDelete(args[0]).then(() => {
-      message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+      message.channel.send(`Cleared ${args[0]} messages.`) // .then(msg => msg.delete(5000));
     });
   });
 }
