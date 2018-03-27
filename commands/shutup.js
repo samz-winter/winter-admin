b7d8027f1d85534a7cc74447a04734b3c6e66b34
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-27 10:11:02 -0500

    Update shutup.js

--- commands/shutup.js
+++ commands/shutup.js
@@ -12,11 +12,20 @@ module.exports.run = async (bot, message, args) => {
   if(!muteRole) return message.reply('I cannot find a `@shutup` role.').catch(console.error);
   if(message.mentions.users.size < 1) return message.reply('You must mention someone to shutup.').catch(console.error);
 
+  let addShutup = message.guild.member(user).addRole(muteRole);
+  
+  message.delete().then(() => {
+    message.channel.send(`2 minutes in the ${penaltyBox}.`).then(() => {
+      setInterval(addShutup, 10 * 1000);
+    });
+  });
+  
+  /*
   message.delete().then(() => {
     message.guild.member(user).addRole(muteRole).then(() => {
       message.channel.send(`2 minutes in the ${penaltyBox}.`);
     });
-  });
+  });  */
 
   setTimeout(function() {
     message.guild.member(user).removeRole(muteRole);
