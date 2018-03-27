f1881f05521420c3b2be98539ed9a127faf16232
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-27 07:52:53 -0500

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -2,6 +2,7 @@ const Discord = module.require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
   if(!args[0]) return message.channel.send("Enter number of messages to clear.");
+  message.delete();
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
   });
