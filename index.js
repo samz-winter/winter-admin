5b1ec925d8d2fe31fd7671ccd98c67e57788302e
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-27 07:17:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const bot = new Discord.Client();
 bot.commands = new Discord.Collection();
-const prefix = '!';
+const prefix = require('./config.json').prefix;
 const allowedUsers = require('./config.json').allowedUsers;
 const responses = require('./commands/textblocks/responses.js').responses;
 
@@ -19,7 +19,6 @@ fs.readdir("./commands/", (err, files) => {
     let props = require(`./commands/${f}`);
     console.log(`${f} loaded!`);
     bot.commands.set(props.help.name, props);
-
   });
 });
 
@@ -35,35 +34,9 @@ bot.on('message', async message => {
   let args = message.content.slice(prefix.length).trim().split(/ +/g);
   let cmd = args.shift().toLowerCase();
 
-  let winterAdmin = bot.users.get("419686533038080003");
-  let winterBot = bot.users.get("420054493250584586");
-
-  let samzID = '111980412296839168';
-  let benzID = '213091147059429393';
-  // let officerRole = message.guild.roles.get("211143245445464064");
-  let samzUser = bot.users.get("111980412296839168");
-  
-  let errorText = `<@!` + message.author.id + `>` + ` Please DM ${winterBot} if you need help.`
-
   if(!msg.startsWith(prefix)) return;
   if(message.author.bot) return;
-  // if message contains command prefix and author not winterOfficer
-  // if(msg.startsWith(prefix) && !message.member.roles.has("211143245445464064")) return; // prefix and not winterOfficer
-  // if(msg.startsWith(prefix) && !bot.users.get("111980412296839168")) return; // prefix and not samzgr8
   if(msg.startsWith(prefix) && !allowedUsers.includes(message.author.id)) return; // prefix and not allowed user IDs
-
-/* NOT WORKING
-  // if message mentions winter-admin and author not samzgr8
-  if(msg.startsWith.user.id === '419686533038080003') {
-    // if(message.author.id !== benzID) {
-    if(message.author.id !== samzID) {
-      // message.author.send({embed: embed});
-      // message.channel.send(`<@!` + message.author.id + `>` + ` **You have encountered an H2IK error! Check your DMs**`);
-      message.channel.send(errorText);
-      return;
-    }
-  }
-*/
 
   try {
     let commandFile = require(`./commands/${cmd}.js`);
