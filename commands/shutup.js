dea52c03046f7af1cc97a38ce28a4c92e5f086f0
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-27 08:20:33 -0500

    Update shutup.js

--- commands/shutup.js
+++ commands/shutup.js
@@ -3,18 +3,21 @@ const Discord = module.require('discord.js');
 module.exports.run = async (bot, message, args) => {
   let reason = args.slice(1).join(' ');
   let user = message.mentions.users.first();
-  let adminLog = message.guild.channels.get("423825546087235585"); // #poked_the_bear
+  let adminLog = message.guild.channels.get("423825546087235585"); // #penalty_box
   let muteRole = message.guild.roles.get("420581883638972432"); // @shutup
 
   if(!adminLog) return message.reply('I cannot find an Admin Log channel.').catch(console.error);
-  if(!muteRole) return message.reply('I cannot find a Mute role.').catch(console.error);
-  // if(reason.length < 1) return message.reply('You must enter a reason.').catch(console.error);
-  if(message.mentions.users.size < 1) return message.reply('You must mention someone to mute.').catch(console.error);
+  if(!muteRole) return message.reply('I cannot find a `@shutup` role.').catch(console.error);
+  if(message.mentions.users.size < 1) return message.reply('You must mention someone to shutup.').catch(console.error);
 
-  message.guild.member(user).addRole(muteRole).then(() => {
+  user.addRole(muteRole).then(() => {
     message.channel.send(`Now ${user} knows what happens when you annoy an officer.`);
     adminLog.send(`${user} has been told to shut-up by ` + `<@!` + message.author.id + `>`);
   });
+
+  setTimeout(function() {
+  message.guild.member(user).removeRole(muteRole);
+  } ms(10000));
 }
 
 module.exports.help = {
