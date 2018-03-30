486c3086b86e4f5f710dad3645a828ea9d2d564f
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-30 10:47:32 -0500

    Create force-role.js

---
+++ commands/force-role.js
@@ -0,0 +1,18 @@
+const Discord = module.require('discord.js');
+// const responses = require('./textblocks/responses.js').responses;
+
+module.exports.run = async (bot, message, args) => {
+  let msg = message.content.toLowerCase();
+  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  let reason = args.slice(0).join(' ');
+  let aRole = message.guild.roles.find('name', reason);
+  let directMessage = message.guild.member(user);
+
+  if(!user) return message.reply("User not found");
+  if(!reason) return message.reply("Specifiy a role");
+  if(!aRole) return message.reply("Could not find that role");
+  
+  if(user.roles.has(aRole.id));
+  await(user.addRole(aRole.id));
+
+}
