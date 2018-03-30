2036f3c164413e2f87d81158227f032ec58af544
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-30 12:53:17 -0500

    Update no_mentions.js

--- commands/no_mentions.js
+++ commands/no_mentions.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let member = message.mentions.members.first();
   let aRole = message.guild.roles.find('name', 'no_mentions');
 
-  if(!user) return message.reply("User not found");
+  if(!member) return message.reply("User not found");
   if(!aRole) return message.reply("Could not find that role");
 
   member.addRole(aRole.id);
