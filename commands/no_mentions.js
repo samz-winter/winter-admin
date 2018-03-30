04e3db26b5963c0367cb04351526694b7de72d53
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-30 12:51:09 -0500

    Update no_mentions.js

--- commands/no_mentions.js
+++ commands/no_mentions.js
@@ -1,20 +1,13 @@
 const Discord = module.require('discord.js');
-// const responses = require('./textblocks/responses.js').responses;
 
 module.exports.run = async (bot, message, args) => {
-  let msg = message.content.toLowerCase();
-  let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  let aRole = message.guild.roles.find('name', 'no_mentions');
-  let directMessage = message.guild.member(user);
-  
-  console.log(`user is ${user}`);
+  let member = message.mentions.members.first();
+  let aRole = message.guild.roles.find('name', no_mentions);
 
-  if(!message.member.hasPermission("MANAGE_ROLES")) return message.reply("no permissions");
   if(!user) return message.reply("User not found");
   if(!aRole) return message.reply("Could not find that role");
-  
-  if(user.roles.has(aRole.id));
-  await(user.addRole(aRole.id));
+
+  member.addRole(aRole.id);
 }
 
 module.exports.help = {
