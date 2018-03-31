050a2e65dc3ac8e5adaa29157049465a2cc4f70a
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-31 14:13:02 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -6,8 +6,7 @@ module.exports.run = async (bot, message, args) => {
   let inviteLinkText = '★  https://discord.gg/C6y5wSM  ★\n'
 + '```diff\n- Invite link updated MARCH.26.2018\n```'
   
-  let inviteText = `The above invite link may be shared as part of any recruiting you are doing, whether amongst friends or openly (Reddit, etc). This invite directs new members to our cool ${welcomeChat} channel which acts as a splash page for the clan and server. From there, they can click through to any other public channel.\n\n`
-+ '**This is the only invite link for the server.** *If you see anyone in the clan asking for an invite link, direct them here.* This link will be refreshed every now and then for security. Check back before sharing.\n\n'
+  let inviteText = '**This is the only invite link for the server.** *If you see anyone in the clan asking for an invite link, direct them here.* This link will be refreshed every now and then for security. Check back before sharing.\n\n'
 + '***WARNING:*** *Using this link to re-invite someone who has been kicked/banned from the server will result in your removal as well.*'
   
   let embed = new Discord.RichEmbed()
