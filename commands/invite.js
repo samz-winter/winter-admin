4e699333874facc8d3bc5c40866b9ae502f92605
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-31 14:13:52 -0500

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let welcomeChat = message.guild.channels.get("413690580410040331")
   
   let inviteLinkText = '★  https://discord.gg/C6y5wSM  ★\n'
-+ '```diff\n- Invite link updated MARCH.26.2018\n```'
++ '```diff\n- Invite link updated MARCH.31.2018\n```'
   
   let inviteText = '**This is the only invite link for the server.** *If you see anyone in the clan asking for an invite link, direct them here.* This link will be refreshed every now and then for security. Check back before sharing.\n\n'
 + '***WARNING:*** *Using this link to re-invite someone who has been kicked/banned from the server will result in your removal as well.*'
