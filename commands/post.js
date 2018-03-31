d5a5f4a24a5fa0275a26e0cbb0c5c57d37babc48
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-03-31 16:05:45 -0500

    Update post.js

--- commands/post.js
+++ commands/post.js
@@ -1,7 +1,7 @@
 /*
-Title: infoblock.js
-Description: combined text block commands for static posts
-Usage: !infoblock {chat_room}
+Title: post.js
+Description: combined post block commands for static posts
+Usage: !post {chat_room}
 */
 const Discord = module.require('discord.js');
 const responses = require('./textblocks/responses.js').responses;
@@ -12,45 +12,45 @@ module.exports.run = async (bot, message, args) => {
   let reason = args.slice(0).join(' ');
   let adminLog = message.guild.channels.get("423825546087235585"); // #admin_log
   let directMessage = message.guild.member(user);
-  // let inviteLink = '★  https://discord.gg/C6y5wSM  ★\n```diff\n- Invite link updated MARCH.31.2018\n```'
+  let inviteLink = '★  https://discord.gg/C6y5wSM  ★\n```diff\n- Invite link updated MAR.31.2018\n```'
 
   if(reason === "testing") {
-    // directMessage.send(guidelinesPing);
     message.delete();
-    message.channel.send(responses.test(bot, message, args))
+    message.channel.send(responses.test(bot, message, args));
+    // directMessage.send(guidelinesPing);
     // adminLog.send(`${user} has been DM'd about community guidelines.`);
     return;
   }else if(reason === "captain") {
     message.delete();
-    message.channel.send(responses.captainPost(bot, message, args))
+    message.channel.send(responses.captainPost(bot, message, args));
     return;
   }else if(reason === "guidelines") {
     message.delete();
-    message.channel.send(responses.guidelinesPost(bot, message, args))
+    message.channel.send(responses.guidelinesPost(bot, message, args));
     return;
   }else if(reason === "invite") {
     message.delete();
-    message.channel.send(responses.invitePost(bot, message, args))
+    message.channel.send(responses.invitePost(bot, message, args));
     return;
   }else if(reason === "invite link") {
     message.delete();
-    message.channel.send('★  https://discord.gg/C6y5wSM  ★\n```diff\n- Invite link updated MAR.31.2018\n```')
+    message.channel.send(inviteLink);
     return;
   }else if(reason === "penalty box") {
     message.delete();
-    message.channel.send(responses.penaltyBoxPost(bot, message, args))
+    message.channel.send(responses.penaltyBoxPost(bot, message, args));
     return;
   }else if(reason === "timeout") {
     message.delete();
-    message.channel.send(responses.timeoutPost(bot, message, args))
+    message.channel.send(responses.timeoutPost(bot, message, args));
     return;
   }else if(reason === "under review") {
     message.delete();
-    message.channel.send(responses.underReviewPost(bot, message, args))
+    message.channel.send(responses.underReviewPost(bot, message, args));
     return;
   }else if(reason === "welcome") {
     message.delete();
-    message.channel.send(responses.welcomePost(bot, message, args))
+    message.channel.send(responses.welcomePost(bot, message, args));
     return;
   }
 }
@@ -58,30 +58,3 @@ module.exports.run = async (bot, message, args) => {
 module.exports.help = {
   name: "post"
 }
-
-/*
-captain.js      || captainPost
-guidelines.js   || guidelinesPost
-bot-help.js     || 
-invite.js       || inviteLink / invitePost
-poked.js        || shutupPost
-timeout.js      || timeoutPost
-under-review.js || underReviewPost
-welcome.js      || welcomePost
-*/
-
-/*
-// post #announcements invite embed
-  let embed = new Discord.RichEmbed()
-    .setColor(0x37a0dc)
-    .setDescription(postInviteText);
-    message.channel.send(postInviteLink, {embed: embed});
-
-// post #shutup embed
-  let embed = new Discord.RichEmbed()
-    .setColor(0x37a0dc)
-    .setDescription(pokedText)
-    .setImage("https://www.reactiongifs.com/r/ksu.gif")
-    .setFooter(pokedFooter);
-    message.channel.send({embed: embed});
-*/
